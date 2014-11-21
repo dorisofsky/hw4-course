c4d9187a12a3658d0bb5cec2c7154e6c33ecbc44
dorisofsky <dorisofsky@gmail.com>
2014-11-21 21:32:44 +0800

    e

--- js/app.js
+++ js/app.js
@@ -68,7 +68,7 @@ var loadPagesInfo = function(pages){
       $page.find('.likes').text(response.likes);
       FB.api(item.id+'/picture?type=large', function(response){
         // 塞資料到 html 中
-        $page.find('.thumbnail img').attr('src',response.data.url)
+        $page.find('.thumbnail img').attr('src',response.data.url);
         $page.appendTo(current);
         counter++;
         // 塞完資料以後處理一下斷行
